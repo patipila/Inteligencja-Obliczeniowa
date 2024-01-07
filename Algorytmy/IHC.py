a59aff4b0e1cd47c0f4851c6daeb5e2fb690ce17
MagdaWnuk <mwnuk@student.agh.edu.pl>
2024-01-07 01:32:27 +0100

    IHC

--- Algorytmy/IHC.py
+++ Algorytmy/IHC.py
@@ -1,49 +1,118 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import random
 
-def losowe_rozwiazanie(dane):
-    return np.random.permutation(len(dane))
-
-def oblicz_odleglosc(trasa, dane):
+def oblicz_odl(kolejnosc, dane):
     suma = 0
-    for i in range(len(trasa) - 1):
-        suma += dane.iloc[trasa[i], trasa[i+1]]
-    return suma
+    koniec = len(dane)
 
-def wspinaczka_z_multistartem(dane, liczba_startow, liczba_iteracji):
-    najlepsza_trasa = None
-    najlepsza_odleglosc = float('inf')
+    for i in range(koniec):
+        if i < koniec - 1:
+            suma += dane.iloc[kolejnosc[i], kolejnosc[i + 1]]
+        else:
+            suma += dane.iloc[kolejnosc[i], kolejnosc[0]]
 
-    for _ in range(liczba_startow):
-        trasa = losowe_rozwiazanie(dane)
+    return float(suma)
+
+def randomowa_kolejnosc(liczba_miast):
+    kolejnosc = random.sample(range(liczba_miast), liczba_miast)
+    return kolejnosc
+
+def swap(seq):
+    swap_indices = random.sample(range(len(seq)), 2)
+    new_seq = seq.copy()
+    new_seq[swap_indices[0]], new_seq[swap_indices[1]] = new_seq[swap_indices[1]], new_seq[swap_indices[0]]
+    return new_seq
+
+def insert(seq):
+    insertion_indices = sorted(random.sample(range(len(seq)), 2))
+    moved_city = seq.pop(insertion_indices[0])
+    seq.insert(insertion_indices[1] - 1, moved_city)
+    return seq
+
+def reverse(seq):
+    reverse_indices = sorted(random.sample(range(len(seq)), 2))
+    new_seq = seq.copy()
+    new_seq[reverse_indices[0]:reverse_indices[1] + 1] = reversed(new_seq[reverse_indices[0]:reverse_indices[1] + 1])
+    return new_seq
+
+def hill_climbing_with_multi_start(dane, liczba_iteracji, num_starts):
+    liczba_miast = len(dane)
+    best_kolejnosc = None
+    best_odl = float('inf')
+
+    funkcje_sasiedztwa = {
+        'swap': swap,
+        'insert': insert,
+        'reverse': reverse
+    }
+
+    for _ in range(num_starts):
+        aktualna_kolejnosc = randomowa_kolejnosc(liczba_miast)
+        aktualna_odl = oblicz_odl(aktualna_kolejnosc, dane)
 
         for _ in range(liczba_iteracji):
-            nowa_trasa = trasa.copy()
-            indeks1, indeks2 = np.random.choice(len(trasa), 2, replace=False)
-            nowa_trasa[indeks1], nowa_trasa[indeks2] = nowa_trasa[indeks2], nowa_trasa[indeks1]
+            nazwa_sasiedztwa, funkcja_sasiedztwa = random.choice(list(funkcje_sasiedztwa.items()))
+            nowa_kolejnosc = funkcja_sasiedztwa(aktualna_kolejnosc)
+            nowa_odl = oblicz_odl(nowa_kolejnosc, dane)
 
-            nowa_odleglosc = oblicz_odleglosc(nowa_trasa, dane)
-            if nowa_odleglosc < oblicz_odleglosc(trasa, dane):
-                trasa = nowa_trasa.copy()
+            if nowa_odl < aktualna_odl:
+                aktualna_kolejnosc = nowa_kolejnosc
+                aktualna_odl = nowa_odl
 
-        aktualna_odleglosc = oblicz_odleglosc(trasa, dane)
-        if aktualna_odleglosc < najlepsza_odleglosc:
-            najlepsza_trasa = trasa.copy()
-            najlepsza_odleglosc = aktualna_odleglosc
+        if aktualna_odl < best_odl:
+            best_kolejnosc = aktualna_kolejnosc
+            best_odl = aktualna_odl
 
-    return najlepsza_trasa
+    return {'kolejnosc': best_kolejnosc, 'odl': best_odl}
 
-# Przykładowe dane (ramka danych odległości między miastami)
-dane = pd.DataFrame({
-    'A': [0, 10, 15, 20],
-    'B': [10, 0, 35, 25],
-    'C': [15, 35, 0, 30],
-    'D': [20, 25, 30, 0]
-})
+# Parametry
+distances_data = pd.read_excel("C:/Users/magda/Documents/GitHub/Inteligencja-Obliczeniowa/Dane/Przykład_TSP_29.xlsx",header=None)
+liczba_iteracji = 10
+num_starts = 10
 
-liczba_startow = 5
-liczba_iteracji = 1000
+# Implementacja funkcji
+for nazwa_sasiedztwa in ['swap', 'insert', 'reverse']:
+    print(f"Metoda generowania sąsiedztwa: {nazwa_sasiedztwa}")
+    result = hill_climbing_with_multi_start(distances_data, liczba_iteracji, num_starts)
+    print(f"Najlepsza kolejnosc: {result['kolejnosc']}")
+    print(f"Długość trasy: {result['odl']}\n")
 
-najlepsza_trasa = wspinaczka_z_multistartem(dane, liczba_startow, liczba_iteracji)
-print("Najlepsza trasa:", najlepsza_trasa)
-print("Najlepsza odległość:", oblicz_odleglosc(najlepsza_trasa, dane))
+'''
+def hill_climbing(dane, liczba_iteracji):
+    liczba_miast = len(dane)
+    naj_kolejnosc = None
+    naj_odl = float('inf')
+
+    for _ in range(liczba_iteracji):
+        aktualna_kolejnosc = randomowa_kolejnosc(liczba_miast)
+        aktualna_odl = oblicz_odl(aktualna_kolejnosc, dane)
+
+        while True:
+            nowa_kolejnosc = randomowa_kolejnosc(liczba_miast)
+            nowa_odl = oblicz_odl(nowa_kolejnosc, dane)
+
+            if nowa_odl < aktualna_odl:
+                aktualna_kolejnosc = nowa_kolejnosc
+                aktualna_odl = nowa_odl
+            else:
+                break  # Wyjdź z pętli wspinaczki, jeśli nie można poprawić trasy
+
+        if aktualna_odl < naj_odl:
+            naj_kolejnosc = aktualna_kolejnosc
+            naj_odl = aktualna_odl
+
+    return naj_kolejnosc, naj_odl
+
+# Przykładowe dane - macierz odległości między miastami
+dane = pd.read_excel("C:/Users/magda/Documents/GitHub/Inteligencja-Obliczeniowa/Dane/Przykład_TSP_29.xlsx",header=None)
+
+liczba_iteracji = 100
+
+# Uruchom algorytm wspinaczki z multistartem
+naj_kolejnosc, naj_odl = hill_climbing(dane, liczba_iteracji)
+
+# Wyświetl wyniki
+print("Najlepsza kolejnosc:", naj_kolejnosc)
+print("Długość trasy:", naj_odl)
+'''
--- Algorytmy/proby.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import random
-
-dane = pd.read_excel("C:/Users/magda/Documents/GitHub/Inteligencja-Obliczeniowa/Dane/Dane_TSP_48k.xlsx", sheet_name="dane", header=None)
-dane = dane.iloc[:, 1:]
-print(dane)
